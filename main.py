6b6b3a2120ed3755136899d1d4a6924aeb257868
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-04-08 11:39:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from _datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "myemail@email.com"
+MY_PASSWORD = "my_pwd"
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row  for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("outlook.office365.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{contents}")
